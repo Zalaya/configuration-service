f1f10f489bdf6414fa80f7a55e1119d2be5c734b
Zalaya <enol.corzal@gmail.com>
2024-10-29 22:52:10 +0100

    refactor(structure): change the structure and name of the project and refactor the code

--- server/main.lua
+++ server/main.lua
@@ -1,20 +1,13 @@
 local localProperties = {}
 local subscribedModules = {}
 
-TriggerEvent("zombie-system:configuration-service:subscribe", "configuration-service")
-
-RegisterNetEvent("zombie-system:configuration-service:configuration-updated")
-AddEventHandler("zombie-system:configuration-service:configuration-updated", function(remoteProperties)
-    localProperties = remoteProperties
-end)
-
-RegisterNetEvent("zombie-system:configuration-service:subscribe")
-AddEventHandler("zombie-system:configuration-service:subscribe", function(subscribedModule)
+RegisterNetEvent("configuration-service:subscribe")
+AddEventHandler("configuration-service:subscribe", function(subscribedModule)
     if not subscribedModules[subscribedModule] then
         subscribedModules[subscribedModule] = true
     end
 
-    TriggerEvent("zombie-system:" .. subscribedModule .. ":configuration-updated", localProperties)
+    TriggerEvent(subscribedModule .. ":properties-updated", localProperties)
 end)
 
 Citizen.CreateThread(function()
@@ -30,11 +23,11 @@ Citizen.CreateThread(function()
                 localProperties = remoteProperties
 
                 for subscribedModule in pairs(subscribedModules) do
-                    TriggerEvent("zombie-system:" .. subscribedModule .. ":configuration-updated", localProperties)
+                    TriggerEvent(subscribedModule .. ":properties-updated", localProperties)
                 end
             end
         end)    
 
-        Citizen.Wait(localProperties["zombie-system.configuration-service.database-polling-interval"] or 5000)
+        Citizen.Wait(5000)
     end
 end)
