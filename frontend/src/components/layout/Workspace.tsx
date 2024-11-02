71eabed862ab5d3ce0659da410aec8a9bbf9115d
Zalaya <enol.corzal@gmail.com>
2024-11-02 20:38:58 +0100

    refactor(workspace): change the workspace color to white and add a shadow

--- frontend/src/components/layout/Workspace.tsx
+++ frontend/src/components/layout/Workspace.tsx
@@ -8,7 +8,7 @@ export const Workspace = ({ className = "" }: WorkspaceProps) => {
     const { getWorkspace } = useNavigation();
 
     return (
-        <div className={`h-full w-full bg-gray-300 rounded-lg p-3 select-none ${className}`}>
+        <div className={`h-full w-full bg-white rounded-lg p-3 select-none shadow-lg ${className}`}>
             {getWorkspace()}
         </div>
     )
