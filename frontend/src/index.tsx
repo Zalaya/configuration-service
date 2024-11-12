6291cfed5a62b44abfb92cf68fd16d671ad3486f
Zalaya <enol.corzal@gmail.com>
2024-11-12 19:22:39 +0100

    feat(components): add the react-router-dom dependency and some layout components that will be used in the future

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
-import App from './components/App.tsx';
+import App from './layout/App.tsx';
 
 import "./styles/index.css";
 
