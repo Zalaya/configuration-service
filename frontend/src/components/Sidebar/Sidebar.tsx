62d7270b6c5652b409134e6e43d09d727d2dc3eb
Zalaya <enol.corzal@gmail.com>
2024-11-13 21:58:15 +0100

    refactor(click): remove the handler from the sidebar

--- frontend/src/components/Sidebar/Sidebar.tsx
+++ frontend/src/components/Sidebar/Sidebar.tsx
@@ -7,16 +7,10 @@ import { MouseEvent, useState } from "react";
 const Sidebar = () => {
   const [isMinimized, setIsMinimized] = useState<boolean>(false);
 
-  const handleClick = (event: MouseEvent) => {
-    if (event.target === event.currentTarget) {
-      setIsMinimized(isMinimized => !isMinimized);
-    };
-  };
-
   return (
     <div
       className={`h-full bg-white rounded-lg p-3 flex flex-col gap-1 transition-all duration-200 ${!isMinimized ? "w-1/6" : "w-[4.5em]"}`}
-      onClick={handleClick}
+      onClick={(event: MouseEvent) => (event.target === event.currentTarget) ? setIsMinimized(isMinimized => !isMinimized) : undefined}
     >
       <Item icon={LayersOutlinedIcon} label={!isMinimized ? "Modules" : undefined} to="/modules" />
       <Item icon={AutoAwesomeMotionOutlinedIcon} label={!isMinimized ? "Properties" : undefined} to="/properties" />
