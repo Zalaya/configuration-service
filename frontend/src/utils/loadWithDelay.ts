91444399c3f7e3c05294aafc18af00178a526b84
Zalaya <enol.corzal@gmail.com>
2024-11-14 19:21:48 +0100

    refactor(utils): remove the testing load with delay function and its implementations

--- frontend/src/utils/loadWithDelay.ts
+++
@@ -1,7 +0,0 @@
-export function loadWithDelay<T>(importFunc: () => Promise<{ default: T }>, delay: number): Promise<{ default: T }> {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve(importFunc());
-    }, delay);
-  });
-}
