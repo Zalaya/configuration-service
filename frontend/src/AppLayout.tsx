67ad21d030f9d462a55c9b79317303989b4d9d6f
Zalaya <enol.corzal@gmail.com>
2024-11-23 18:44:30 +0100

    refactor(structure): remove the old structure just to remake it

--- frontend/src/AppLayout.tsx
+++ frontend/src/AppLayout.tsx
@@ -1,16 +1,6 @@
-import AppRoutes from "@/routes/AppRoutes";
-
-import Panel from "@/components/Layout/Panel";
-import Sidebar from "@/components/Layout/Sidebar";
-
 const AppLayout = () => {
   return (
-    <div className="h-screen w-screen flex gap-3 bg-gray-200 p-3">
-      <Sidebar />
-      <Panel>
-        <AppRoutes />
-      </Panel>
-    </div>
+    <h1>Hello World!</h1>
   );
 };
 
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,6 +1,5 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
 
 import AppLayout from "@/AppLayout";
 
@@ -8,8 +7,6 @@ import "@/styles/index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
-      <AppLayout />
-    </BrowserRouter>
+    <AppLayout />
   </StrictMode>
 );
