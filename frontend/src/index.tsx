b817c991504a409fba30e659fdc67428df120f53
Zalaya <enol.corzal@gmail.com>
2024-11-27 18:04:15 +0100

    feat(docker): add the dockerfile, docker-compose and some documentation files

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,10 +1,12 @@
-import { StrictMode } from "react"
+import { Fragment, StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 
 import "@/styles/index.css"
 
+const Wrapper = import.meta.env.VITE_REACT_STRICT_MODE === "true" ? StrictMode : Fragment;
+
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
+  <Wrapper>
     <App />
-  </StrictMode>
+  </Wrapper>
 );
