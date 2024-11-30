10d0086caf02516a50502b737b0f3d5b70d7457f
Zalaya <enol.corzal@gmail.com>
2024-11-30 01:51:37 +0100

    refactor: add some configuration using the env files

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -5,9 +5,7 @@ import App from "@/App";
 
 import "@/styles/index.css";
 
-const Wrapper = import.meta.env.VITE_REACT_STRICT_MODE === "true" ? StrictMode : Fragment;
-
-console.log(import.meta.env.VITE_REACT_STRICT_MODE);
+const Wrapper = import.meta.env.VITE_STRICT_MODE === "true" ? StrictMode : Fragment;
 
 createRoot(document.getElementById("root")!).render(
   <Wrapper>
