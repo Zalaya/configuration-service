75485a8716b50e0e1c2e72e75c6431109bb1cb60
Zalaya <enol.corzal@gmail.com>
2024-11-30 01:42:25 +0100

    feat: add two more .env files, one for production and one for development

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -7,6 +7,8 @@ import "@/styles/index.css";
 
 const Wrapper = import.meta.env.VITE_REACT_STRICT_MODE === "true" ? StrictMode : Fragment;
 
+console.log(import.meta.env.VITE_REACT_STRICT_MODE);
+
 createRoot(document.getElementById("root")!).render(
   <Wrapper>
     <App />
