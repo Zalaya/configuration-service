21f2b72a85d67363719fb1e094641f43b8b05e6e
Zalaya <enol.corzal@gmail.com>
2024-11-30 01:12:01 +0100

    feat: add the home direction for the routes

--- frontend/src/routes/routes.tsx
+++ frontend/src/routes/routes.tsx
@@ -1,9 +1,13 @@
-import { RouteObject } from "react-router-dom";
+import { Navigate, RouteObject } from "react-router-dom";
 
 import authRoutes from "@/layouts/auth/routes/routes";
 import dashboardRoutes from "@/layouts/dashboard/routes/routes";
 
 const appRoutes: RouteObject[] = [
+  {
+    path: "/",
+    element: <Navigate to="/dashboard/modules" replace />
+  },
   ...authRoutes,
   ...dashboardRoutes
 ];
