0f4c082d86597ae50c0ac929758896f85b165346
Zalaya <enol.corzal@gmail.com>
2024-12-02 23:00:09 +0100

    feat: add more effects for the hover in the items

--- frontend/src/components/Sidebar/Item.tsx
+++ frontend/src/components/Sidebar/Item.tsx
@@ -7,7 +7,7 @@ type ItemProps = {
 
 const Item = ({ icon: Icon, text }: ItemProps) => {
   return (
-    <div className="hover:bg-gray-200 transition-colors duration-200 cursor-pointer flex gap-2 p-3 rounded-lg items-center">
+    <div className="hover:bg-gray-200 hover:text-blue-700 transition-colors duration-200 cursor-pointer flex gap-2 p-3 rounded-lg items-center">
       <Icon />
       <span className="font-semibold text-base">
         {text}
